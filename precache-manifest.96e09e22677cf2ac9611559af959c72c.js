1ba1cd99f0b62a3595ceff64361b9e81d933c753
abdulhalimmanik <abdulhalimmanik@users.noreply.github.com>
2020-05-05 16:22:33 +0600

    Updates

---
+++ precache-manifest.96e09e22677cf2ac9611559af959c72c.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = [
+  {
+    "revision": "160fddff3ca71887ebf8da3929244379",
+    "url": "/ecommerce/static/media/productsBcg.160fddff.jpeg"
+  },
+  {
+    "revision": "03d1d42c229b0211fdc2",
+    "url": "/ecommerce/static/css/main.22df0c6d.chunk.css"
+  },
+  {
+    "revision": "e176d1401fcedba87ff8",
+    "url": "/ecommerce/static/js/runtime~main.8409c7fe.js"
+  },
+  {
+    "revision": "2b362308d9a07ef7d73f4865cbd132ab",
+    "url": "/ecommerce/static/media/logo.2b362308.svg"
+  },
+  {
+    "revision": "ee411297b9c0d077e17d",
+    "url": "/ecommerce/static/js/2.5840a6b2.chunk.js"
+  },
+  {
+    "revision": "7709c10e8c30753537d94a6b69d7542a",
+    "url": "/ecommerce/static/media/aboutBcg.7709c10e.jpeg"
+  },
+  {
+    "revision": "984874d19d1cdef8dcee5fcea55c082d",
+    "url": "/ecommerce/static/media/mainBcg.984874d1.jpeg"
+  },
+  {
+    "revision": "03d1d42c229b0211fdc2",
+    "url": "/ecommerce/static/js/main.f616b5c8.chunk.js"
+  },
+  {
+    "revision": "945afb997e0731b038ed58fadacfeb3f",
+    "url": "/ecommerce/static/media/contactBcg.945afb99.jpeg"
+  },
+  {
+    "revision": "9675841294261b773267d557f531a8ff",
+    "url": "/ecommerce/static/media/singleProductBcg.96758412.jpeg"
+  },
+  {
+    "revision": "46143c2f25bc4667fac18f5924288217",
+    "url": "/ecommerce/static/media/storeBcg.46143c2f.jpeg"
+  },
+  {
+    "revision": "0819a81634391f8518fe325378b8f994",
+    "url": "/ecommerce/static/media/defaultBcg.0819a816.jpeg"
+  },
+  {
+    "revision": "ee411297b9c0d077e17d",
+    "url": "/ecommerce/static/css/2.c0b7f447.chunk.css"
+  },
+  {
+    "revision": "38fa3eafd57225c31162611a1b12f293",
+    "url": "/ecommerce/index.html"
+  }
+];
